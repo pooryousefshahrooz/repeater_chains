881469ef831050ace66f62692447539acadfec83
Shahrooz <sh.pooryousef2@gmail.com>
2024-02-17 04:30:29 +0000

    figure 2

--- figures_paper/utils.py
+++ figures_paper/utils.py
@@ -45,10 +45,14 @@ def T_parallel_no_cutoff(τ_coh, mu_link, F_link, links, cct= True, Nmax=10000):
         links = np.array(links)
     τs = links/c
     Ns = np.zeros((N_links,Nmax))
-    Ns[0,:] = (2*F_geo(np.random.rand(Nmax),p_link*Trans(links[0]))-1)*τs[0]
+    # Ns[0,:] = (2*F_geo(np.random.rand(Nmax),p_link*Trans(links[0]))-1)*τs[0]
+    p_i = p_link*Trans(links[0])
+    Ns[0,:] = (2*F_geo(np.random.uniform(low=p_i, high=1, size=(Nmax,)),p_i)-1)*τs[0]
     for i in range(1,N_links):
-        Ns[i,:] = 2*F_geo(np.random.rand(Nmax),p_link*Trans(links[i]))*τs[i]
-    
+        # Ns[i,:] = 2*F_geo(np.random.rand(Nmax),p_link*Trans(links[i]))*τs[i]
+        p_i = p_link*Trans(links[i])
+        Ns[i,:] = 2*F_geo(np.random.uniform(low=p_i, high=1, size=(Nmax,)),p_i)*τs[i]
+
     Ts = np.zeros((N_links-1,Nmax))
     Ts[0,:] = np.abs(Ns[1,:]-Ns[0,:])+ 2*τs[1]
     for i in range(1,N_links-1):
@@ -120,7 +124,7 @@ def T_sequential_no_cutoff(τ_coh, mu_link, F_link,links):
 
     return raw_rate, skr, F_e2e
 
-def T_parallel_cutoff(τ_cut, τ_coh, mu_link, F_link,links, cct= True, Nmax=100000):
+def T_parallel_cutoff_slow(τ_cut, τ_coh, mu_link, F_link,links, cct= True, Nmax=100000):
     """ Runs Monte-Carlo iterations to calculate performance metrics for asynchronous parallel scheme
     inputs:
         τ_coh: coherence time of quantum memories
@@ -144,7 +148,9 @@ def T_parallel_cutoff(τ_cut, τ_coh, mu_link, F_link,links, cct= True, Nmax=100
     τs = links/c
     Ns = np.zeros((N_links,Nmax))
     for i in range(N_links):
-        Ns[i,:] = F_geo(np.random.rand(Nmax),p_link*Trans(links[i]))
+        # Ns[i,:] = F_geo(np.random.rand(Nmax),p_link*Trans(links[i]))
+        p_i = p_link*Trans(links[i])
+        Ns[i,:] = F_geo(np.random.uniform(low=p_i, high=1, size=(Nmax,)),p_i)
 
     Tswap = np.zeros((N_links-1,Nmax))
     Tswap_cc = np.zeros((N_links-1,Nmax))
@@ -180,6 +186,81 @@ def T_parallel_cutoff(τ_cut, τ_coh, mu_link, F_link,links, cct= True, Nmax=100
     # mean_fail= 0
     # if len(fail_inds)> 0:
     #     mean_fail=  T_fail/len(fail_inds)
+    Ts_A = np.max(Tswap_cc[:,succ_inds],axis=0)- 2*(Ns[0,succ_inds]-1)* τs[0] 
+    Ts_B = np.max(Tswap_cc[:,succ_inds],axis=0)- (2*Ns[-1,succ_inds]-1)* τs[-1] 
+    f_memory_bell = np.mean(np.exp(-(np.sum(tL[:,succ_inds]+tR[:,succ_inds],axis=0)+Ts_A+Ts_B)/τ_coh))
+
+    f_memory_qkd = np.mean(np.exp(-np.sum(tL[:,succ_inds]+tR[:,succ_inds],axis=0)/τ_coh))
+    mu_e2e = mu_link**(2*N_links-1)
+    # secret key rate calculations
+    f_e2e_qkd = 0.5 + 0.5 * (2*F_link-1)**N_links *f_memory_qkd
+    ex = (1 - mu_e2e)/2
+    ez = (1 + mu_e2e)/2 - mu_e2e * f_e2e_qkd
+    skr = raw_rate * (1-h([ex])-h([ez]))
+    #  fidelity of e2e Bell pairs
+    f_e2e_bell = 0.5 + 0.5 * (2*F_link-1)**N_links *f_memory_bell
+    F_e2e = mu_e2e * f_e2e_bell + (1-mu_e2e)/4
+    return raw_rate, skr, F_e2e
+
+
+
+def T_parallel_cutoff(τ_cut, τ_coh, mu_link, F_link,links, cct= True, Nmax=100000):
+    """ Runs Monte-Carlo iterations to calculate performance metrics for asynchronous parallel scheme
+    inputs:
+        τ_coh: coherence time of quantum memories
+        mu_link: parameter in 2qubit depolarizing channel describing noisy link-level entanglement and
+        entanglement swapping error
+        F_link: fidelity of link level entanglement (i.e.,quality of locally generated Bell pairs)
+        links: list of segment (link) lengths in km
+        cct: bool flag to turn on/off classical comm.
+        Nmax: No. of MC iterations (if you see oscillations in secret key curves, you need to try larger numbers)
+
+    outputs:
+        Raw_rate: 1/ expected value of total time for e2e entanglement delivery
+        *** application specific quantities:
+        skr: secret key rate for qkd (does not include idle times of end memories)
+        F_e2e: e2e entanglement fidelity for entanglement distrubtion (does include idle times of end memories)
+    """
+    Nmax = int(Nmax) # to make sure Nmax is an integer
+    N_links = len(links) # number of links, i.e. no. of repeaters + 1
+    if type(links) != np.ndarray:
+        links = np.array(links)
+    τs = links/c
+    Ns = np.zeros((N_links,Nmax))
+    for i in range(N_links):
+        # Ns[i,:] = F_geo(np.random.rand(Nmax),p_link*Trans(links[i]))
+        p_i = p_link*Trans(links[i])
+        Ns[i,:] = F_geo(np.random.uniform(low=p_i, high=1, size=(Nmax,)),p_i)
+
+    Tswap = np.zeros((N_links-1,Nmax))
+    Tswap_cc = np.zeros((N_links-1,Nmax))
+    tL = np.zeros((N_links-1,Nmax))
+    tR = np.zeros((N_links-1,Nmax))
+    for i in range(N_links-1):
+        Tswap[i,:] = np.max(np.stack((2*Ns[i+1,:]*τs[i+1],(2*Ns[i,:]-1)*τs[i]),axis=0), axis=0)
+        tL[i,:] = Tswap[i,:]- (2*Ns[i,:]-1)*τs[i]
+        tR[i,:] = Tswap[i,:]- 2*(Ns[i+1,:]-1)*τs[i+1]
+        Tswap_cc[i,:] = Tswap[i,:] + np.sum(τs[:(i+1)])
+
+    indsL = np.argwhere(np.sum( tL <= τ_cut , axis = 0)== N_links-1 )[:,0]
+    indsR = np.argwhere(np.sum( tR <= τ_cut , axis = 0)== N_links-1 )[:,0]
+    succ_inds = np.intersect1d(indsL,indsR)
+    T_succ = np.sum(np.max(Tswap_cc[:,succ_inds],axis=0))           
+    # mean_succ =  T_succ/len(succ_inds)
+    # return mean_succ
+    fail_inds = list(set(list(range(Nmax)))-set(succ_inds))
+
+    comm_time = np.array([np.sum(τs[:(i_r+1)]) for i_r in range(N_links-1)]).reshape((N_links-1, 1))
+    tsL = (tL[:,fail_inds] > τ_cut)* ( ( (2*Ns[:-1,fail_inds]-1) * τs[:-1].reshape((N_links-1, 1)) ) + comm_time )
+    tsR = (tR[:,fail_inds] > τ_cut)* ( (2*(Ns[1:,fail_inds]-1) * τs[1:].reshape((N_links-1, 1)) ) + comm_time )
+    ts = np.concatenate( (tsL,tsR), axis=0)
+    ts[ts==0] = inf
+    T_fail = np.sum( np.min( ts , axis=0 ) +  τ_cut)
+    raw_rate = len(succ_inds)/(T_succ+T_fail)
+    # mean_fail= 0
+    # if len(fail_inds)> 0:
+    #     mean_fail=  T_fail/len(fail_inds)
+
     Ts_A = np.max(Tswap_cc[:,succ_inds],axis=0)- 2*(Ns[0,succ_inds]-1)* τs[0] 
     Ts_B = np.max(Tswap_cc[:,succ_inds],axis=0)- (2*Ns[-1,succ_inds]-1)* τs[-1] 
     f_memory_bell = np.mean(np.exp(-(np.sum(tL[:,succ_inds]+tR[:,succ_inds],axis=0)+Ts_A+Ts_B)/τ_coh))
@@ -247,7 +328,49 @@ def T_sequential_cutoff(τ_cut,τ_coh, mu_link, F_link,links):
 
     # skr, F_e2e = 0, 0
     return raw_rate, skr, F_e2e
+def one_repeater_sequential_cutoff(L1,L2,τ_cut,τ_coh, mu_link, F_link):
+    """ calculates the performance of parallel scheme with one repeater
+    inputs:
+        L1,L2: elementary link lengths *** works for arbitrary L1,L2 ****
+        τ_cut: cut-off time
+        τ_coh: coherence time of quantum memories
+        mu_link: parameter in 2qubit depolarizing channel describing noisy link-level entanglement and
+        entanglement swapping error
+        F_link: fidelity of link level entanglement (i.e.,quality of locally generated Bell pairs)
+    outputs:
+        Raw_rate: 1/ expected value of total time for e2e entanglement delivery
+        *** application specific quantities:
+        skr: secret key rate for qkd (does not include idle times of end memories)
+        F_e2e: e2e entanglement fidelity for entanglement distrubtion (does include idle times of end memories)
+    """
+    τ1 = L1/c
+    τ2 = L2/c
+    m = int(τ_cut/(2*τ2))
+    p1 = p_link*Trans(L1)
+    p2 = p_link*Trans(L2)
+    # Q2m = (1-p2)**m
+    P2m = 1- (1-p2)**m
+    Nm = lambda x: (1-(1+m*x)*(1-x)**m)/x
+    Tm = 2*τ1/(p1*P2m) +  (1/P2m-1)*τ_cut +Nm(p2)/P2m*2*τ2 
 
+    raw_rate = 1/(Tm)
+    # t1L = Nm(p2)/P2m*2*τ2
+    # t1R = 2*τ2
+    f_memory_qkd = p2*np.exp(-4*τ2/τ_coh)/ P2m * (1-(1-p2)**m * np.exp(-2*m*τ2/τ_coh))/(1- (1-p2)*np.exp(-2*τ2/τ_coh) ) 
+    N_links = 2
+    mu_e2e = mu_link**(2*N_links-1)
+    # secret key rate calculations
+    f_e2e_qkd = 0.5 + 0.5 * (2*F_link-1)**N_links *f_memory_qkd
+    ex = (1 - mu_e2e)/2
+    ez = (1 + mu_e2e)/2 - mu_e2e * f_e2e_qkd
+    skr = raw_rate * (1-h([ex])-h([ez]))
+    #  fidelity of e2e Bell pairs
+    τe2e = τ1 + τ2
+    f_memory_bell = np.exp(-3*τe2e/τ_coh) *p2*np.exp(-4*τ2/τ_coh)/ P2m * (1-(1-p2)**m * np.exp(-4*m*τ2/τ_coh))/(1- (1-p2)*np.exp(-4*τ2/τ_coh) ) 
+    f_e2e_bell = 0.5 + 0.5 * (2*F_link-1)**N_links *f_memory_bell
+    F_e2e = mu_e2e * f_e2e_bell + (1-mu_e2e)/4
+    
+    return raw_rate, skr, F_e2e
 def one_repeater_parallel_cutoff(L1,L2,τ_cut,τ_coh, mu_link, F_link, Nmax=100000):
     """ calculates the performance of parallel scheme with one repeater
     inputs:
@@ -269,8 +392,10 @@ def one_repeater_parallel_cutoff(L1,L2,τ_cut,τ_coh, mu_link, F_link, Nmax=1000
     τ2 = L2/c
     p1 = p_link*Trans(L1)
     p2 = p_link*Trans(L2)
-    N1 = F_geo(np.random.rand(Nmax),p1)
-    N2 = F_geo(np.random.rand(Nmax),p2)
+    N1 = F_geo(np.random.uniform(low=p1, high=1, size=(Nmax,)),p1)
+    N2 = F_geo(np.random.uniform(low=p2, high=1, size=(Nmax,)),p2)
+    # N1 = F_geo(np.random.rand(Nmax),p1)
+    # N2 = F_geo(np.random.rand(Nmax),p2)
     Ts = np.max(np.array([(2*N1-1)*τ1,2*N2*τ2]),axis=0) # swap moment
     t1L = Ts-(2*N1-1)*τ1 # elapsed time of leftside repeater's memory 
     t1R = Ts-2*(N2-1)*τ2 # elapsed time of rightside repeater's memory 
